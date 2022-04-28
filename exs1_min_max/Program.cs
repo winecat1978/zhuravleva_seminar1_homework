4c5a30dc7d9b3877671d7a1c5026f6f5f7f2af32
Katherine_20 <mob1911494@gmail.com>
2022-04-28 09:20:46 +0300

    added first task of identifying min and max

---
+++ exs1_min_max/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    Console.WriteLine("max = " + num1);
+    Console.WriteLine("min = " + num2);
+}
+if (num1 < num2) {
+      Console.WriteLine("max = " + num2);
+      Console.WriteLine("min = " + num1);
+}
+if (num1 == num2) {
+     Console.WriteLine(num1 + " = " + num2);
+}

f157a07e317e4085a80fd6899dd934c3e98ce94d
Katherine_20 <mob1911494@gmail.com>
2022-04-28 09:44:27 +0300

    added second task of identifying max among 3 numbers

---
+++ exs2_3numbers_min_max/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num1 < num2) {
+    max = num2;
+    if (num2 > num3) max = num2;
+}
+else
+if (num1 < num3) max = num3;
+
+Console.WriteLine("max = " + max);

e9fbec2bc59eada441ba8976eec9de5e59b7c00d
Katherine_20 <mob1911494@gmail.com>
2022-04-28 09:58:51 +0300

    added third task that checks if a number can be divided by 2 without anything left

---
+++ exs3_if_div2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 0) {
+    Console.WriteLine ("ноль не является ни четным, ни нечетным");
+}
+else
+if (num % 2 == 0) {
+    Console.WriteLine("число " + num + " четное");
+}
+else 
+Console.WriteLine("число " + num + " нечетное");

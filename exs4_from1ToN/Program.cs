e24c8fd1c05ec9bfb229de342ba42cec5c94d173
Katherine_20 <mob1911494@gmail.com>
2022-04-28 10:37:29 +0300

    added fourth task

---
+++ exs4_from1ToN/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+do {
+if (start % 2 == 0) {
+Console.WriteLine(start + ", ");
+start = start + 1;
+} 
+}
+while (start < num);

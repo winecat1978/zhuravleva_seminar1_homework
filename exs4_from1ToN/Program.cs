82f95684bc9e6280eff72641297a55b4881cc55b
Katherine_20 <mob1911494@gmail.com>
2022-04-28 11:00:12 +0300

    finished 4th task

--- exs4_from1ToN/Program.cs
+++ exs4_from1ToN/Program.cs
@@ -2,10 +2,14 @@
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int start = 1;
-do {
-if (start % 2 == 0) {
-Console.WriteLine(start + ", ");
+if (num > 2) {
 start = start + 1;
-} 
+do {
+Console.Write(start + ", ");
+start = start + 2;
 }
-while (start < num);
+while (start < num - 2);
+Console.WriteLine(start + ".");
+}
+else 
+if (num == 2) Console.WriteLine("в промежутке от 1 до 2 не содержится четных чисел");
